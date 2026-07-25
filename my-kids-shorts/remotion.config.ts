44fac8d9e251153113dfa9a36f69aa67024d46e2
abhinay <abhinaypalakurthi00@gmail.com>
2026-07-25 15:56:52 +0530

    Delete my-kids-shorts directory

--- my-kids-shorts/remotion.config.ts
+++
@@ -1,7 +0,0 @@
-// Remotion config file
-import { Config } from '@remotion/cli/config';
-
-Config.setChromiumOpenGlRenderer('swiftshader');
-
-// Entry point that calls registerRoot()
-export const entryPoint = './src/index.ts';
